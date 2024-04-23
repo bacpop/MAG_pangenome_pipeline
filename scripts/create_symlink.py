d5bf512b797f878ad0121aa092d2117c4e1bf31e
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2024-04-23 14:35:07 +0100

    scripts/create_symlink.py

--- scripts/create_symlink.py
+++ scripts/create_symlink.py
@@ -1,6 +1,7 @@
 import glob
 from Bio import SeqIO
 import os
+import shutil
 # inputdir and outputdir need to be without trailing "/"
 
 
@@ -14,4 +15,14 @@ def symlink(list_of_dirs, file_ext, outputdir):
         filename = file.split("/")[-1]
         os.symlink(file, outputdir + "/" + filename)
 
-symlink(snakemake.input.indir, snakemake.params.file_ext, snakemake.output.outputdir)
+def copy(list_of_dirs, file_ext, outputdir):
+    try:
+        os.mkdir(outputdir)
+    except OSError:
+        pass
+    for ann_dirs in list_of_dirs:
+        file = glob.glob(ann_dirs + "/*." + file_ext)[0]
+        filename = file.split("/")[-1]
+        shutil.copyfile(file, outputdir + "/" + filename)
+
+copy(snakemake.input.indir, snakemake.params.file_ext, snakemake.output.outputdir)
